73c2c62d4cc6ae2f6c8cfd099bf6bdea518175ce
Randy Lau <randychilau@gmail.com>
2023-06-04 16:41:14 -0700

    test lighthouse
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -29,7 +29,12 @@ module.exports = {
         "preset": "lighthouse:no-pwa",
         "assertions": {
           "csp-xss": "off",
-          "aria-hidden-focus": "off"
+          "aria-hidden-focus": "off",
+          "categories:performance": ["warn", {"minScore": .90}],
+          "categories:accessibility": ["warn", {"minScore": .90}],
+          "categories:best-practices": ["warn", {"minScore": .90}],
+          "categories:seo": ["warn", {"minScore": .90}],
+          }
         }
       },
       "upload": {
