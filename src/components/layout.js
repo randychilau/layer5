71bfd784938d47dbc1873eb2b14a92b0b93ef85e
Randy Lau <randychilau@gmail.com>
2023-06-05 14:01:02 -0700

    destructure pathname from children
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ import { GlobalStyle } from "../sections/app.style";
 
 const Layout = ({ children }) => {
 
-  const { props: { path } } = children;
+  const { props: { location: { pathname } } } = children;
 
   return (
     <>
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
       <Navigation/>
       {children}
       <ScrollToTopBtn />
-      <Footer path={path}/>
+      <Footer pathname={pathname}/>
     </>
   );
 };
