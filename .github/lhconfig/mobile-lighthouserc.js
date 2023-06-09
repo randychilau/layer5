bf3216eca5296fdbfc786ab9749dd69ba3418d15
Randy Lau <randychilau@gmail.com>
2023-06-09 14:06:03 -0700

    try mobile and desktop lhci
    Signed-off-by: Randy Lau <randychilau@gmail.com>

---
+++ .github/lhconfig/mobile-lighthouserc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "ci": {
+      "collect": {
+        // "settings": {
+        //   "preset": "desktop"
+        // },
+        "staticDistDir": "./public",
+        "url" : [
+                  // "http://localhost/404.html",
+                //  "http://localhost/index.html",
+                //  "http://localhost/404/index.html",
+                 "http://localhost/company/about/index.html?mobile",
+                //  "http://localhost/cloud-native-management/meshery.html",
+                //  "http://localhost/cloud-native-management/meshmap.html",
+                //  "http://localhost/learn/learning-paths.html",
+                //  "http://localhost/learn/service-mesh-books.html",
+                //  "http://localhost/learn/service-mesh-workshops.html",
+                //  "http://localhost/learn.html",
+                //  "http://localhost/blog.html",
+                //  "http://localhost/resources.html",
+                //  "http://localhost/careers.html",
+                //  "http://localhost/community/members.html",
+                //  "http://localhost/community/meshmates.html",
+                //  "http://localhost/community/events.html",
+                //  "http://localhost/community/newcomers.html",
+              ],
+      }
+    }
+  };
