c79810db75933c07e1a042ef6b8a20ed066f0861
Randy Lau <randychilau@gmail.com>
2023-06-09 12:30:20 -0700

    lighthouse config
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -9,7 +9,7 @@ module.exports = {
                   // "http://localhost/404.html",
                 //  "http://localhost/index.html",
                 //  "http://localhost/404/index.html",
-                 "http://localhost/company/about",
+                 "http://localhost/company/about/index.html",
                 //  "http://localhost/cloud-native-management/meshery.html",
                 //  "http://localhost/cloud-native-management/meshmap.html",
                 //  "http://localhost/learn/learning-paths.html",
