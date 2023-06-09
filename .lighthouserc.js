0d7af87ca0a23851cba788dfbb0f507aad41eed5
Randy Lau <randychilau@gmail.com>
2023-06-09 16:58:27 -0700

    lhrc test
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,11 +1,9 @@
 module.exports = {
     "ci": {
       "assert": {
-        "preset": "lighthouse:no-pwa",
         "assertions": {
           "csp-xss": "off",
           "aria-hidden-focus": "off",
-          "unused-css-rules": "warn",
           "categories:performance": ["warn", {"minScore": .90}],
           "categories:accessibility": ["warn", {"minScore": .90}],
           "categories:best-practices": ["warn", {"minScore": .90}],
